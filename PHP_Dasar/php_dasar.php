deeb802ebba0edcc25f855a947fa5bf16c0e1425
WhoIam969 <rizkiberkah107@gmail.com>
2023-03-28 22:32:06 +0700

    Program

---
+++ PHP_Dasar/php_dasar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP Dasar</title>
+    </head>
+    <body>
+        <h1>Belajar PHP Dasar</h1>
+        <?php
+            echo "Hello World";
+        ?>
+        <h1>Menggunakan variable</h1>
+        <?php
+            $nim = "46673592";
+            $nama = 'Rizki';
+            echo "NIM : " . $nim . "<br>";
+            echo "Nama : $nama";
+        ?>
+    </body>
+</html>
